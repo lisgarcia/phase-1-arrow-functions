29135295d79cd36ef76f39a3320552d604625154
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-01 16:59:41 -0400

    redone

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-const divide = function() {
-   let a =2000
-   let b =100
-   return a/b
+let divide = function () {
+   let x=2000
+   let y=100
+   return (x/y)
 }
-divide()
 
-const square = (p1) => p1*p1
-square()
+const square = (x) => {
+   return (x*x)
+}
 
-const add = (pa, pb) => pa + pb
-add()
+const add = (x, y) => {
+   return x+y
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const divide = function() {
+//    let a =2000
+//    let b =100
+//    return a/b
+// }
+// divide()
+
+// const square = (p1) => p1*p1
+// square()
+
+// const add = (pa, pb) => pa + pb
+// add()
